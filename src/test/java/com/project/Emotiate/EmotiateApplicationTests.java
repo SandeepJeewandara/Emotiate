1852068e233937a8af46091f67ab093205ba47fa
Sandeep Jeewandara <sandeepajeewandara@gmail.com>
2026-03-07 12:29:07 +0530

    chore: Initialize EMOTIATE project structure and base configuration

---
+++ src/test/java/com/project/Emotiate/EmotiateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Emotiate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotiateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
